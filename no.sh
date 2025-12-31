092455c904b1da7870e52af2ff4cbea5db5bb0f0
Niklas <dieeinzigenkisten@gmail.com>
2025-12-31 13:27:39 +0100

    Enhance 'no' command with options for output and intervals
    Refactor 'no' command to support various options and improve functionality.

--- no.sh
+++ no.sh
@@ -1,36 +1,105 @@
 #!/bin/sh
-# Author: ihatemustard
-# Flexible "no" command
 
-times=-1
-word="n"
+VERSION="1.0"
 
-# Parse arguments
-while [ $# -gt 0 ]; do
-    case "$1" in
-        --times)
-            shift
-            times="$1"
-            ;;
-        *)
-            word="$1"
-            ;;
-    esac
-    shift
-done
+TEXT="n"
+INTERVAL=0
+TIMES=0
+USE_RANDOM=0
+RANDOM_ITEMS=""
+USE_COUNT=0
+OUTPUT=""
 
-print_word() {
-    echo "$word"
+usage() {
+    echo "Usage: no [text] [options]"
+    echo
+    echo "Options:"
+    echo "  -i, --interval <seconds>   Add a delay between outputs"
+    echo "  -v, --version              Show version info"
+    echo "  -h, --help                 Show this help"
+    echo "  -r, --random <list>        Repeat random strings from comma-separated list"
+    echo "  -c, --count                Prepend counter to each output"
+    echo "  -o, --output <file>        Write output to a file instead of stdout"
+    echo "  -t, --times <n>            Number of times to repeat (0 = infinite)"
+    echo
+    echo "Examples:"
+    echo "  no"
+    echo "  no example -o file.txt"
+    echo "  no --interval 0.5 --times 5"
+    echo "  no --random \"no,nah,nop\" --times 4"
+    echo "  no --count --times 3"
+    echo "  no --output log.txt --times 5"
+    exit 0
 }
 
-if [ "$times" -eq -1 ]; then
-    while true; do
-        print_word
-    done
-else
-    i=0
-    while [ $i -lt "$times" ]; do
-        print_word
-        i=$((i + 1))
-    done
-fi
+pick_random() {
+    LIST="$1"
+    COUNT=$(echo "$LIST" | awk -F',' '{print NF}')
+    RAND_BYTE=$(od -An -N2 -tu2 /dev/urandom | tr -d ' ')
+    INDEX=$(( (RAND_BYTE % COUNT) + 1 ))
+    echo "$LIST" | awk -v i="$INDEX" -F',' '{print $i}'
+}
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -i|--interval)
+            INTERVAL="$2"
+            shift 2
+            ;;
+        -v|--version)
+            echo "no v$VERSION"
+            exit 0
+            ;;
+        -h|--help)
+            usage
+            ;;
+        -r|--random)
+            USE_RANDOM=1
+            RANDOM_ITEMS="$2"
+            shift 2
+            ;;
+        -c|--count)
+            USE_COUNT=1
+            shift
+            ;;
+        -o|--output)
+            OUTPUT="$2"
+            shift 2
+            ;;
+        -t|--times)
+            TIMES="$2"
+            shift 2
+            ;;
+        *)
+            TEXT="$1"
+            shift
+            ;;
+    esac
+done
+
+i=0
+while [ "$TIMES" -eq 0 ] || [ "$i" -lt "$TIMES" ]; do
+    if [ "$USE_RANDOM" -eq 1 ] && [ -n "$RANDOM_ITEMS" ]; then
+        OUT=$(pick_random "$RANDOM_ITEMS")
+    else
+        OUT="$TEXT"
+    fi
+
+    if [ "$USE_COUNT" -eq 1 ]; then
+        PREFIX="$((i+1)): "
+    else
+        PREFIX=""
+    fi
+
+    if [ -n "$OUTPUT" ]; then
+        echo "${PREFIX}${OUT}" >> "$OUTPUT"
+    else
+        echo "${PREFIX}${OUT}"
+    fi
+
+    i=$((i+1))
+    awk_exit=$(echo "$INTERVAL > 0" | awk '{exit !$1}')
+    if [ "$?" -eq 0 ]; then
+        sleep "$INTERVAL"
+    fi
+done
