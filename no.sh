4109721bbeb6ae1cdd71a705b238924b69fb91b4
Niklas <dieeinzigenkisten@gmail.com>
2025-12-31 12:11:26 +0100

    Enhance 'no' command with optional repeat feature
    Added functionality to the 'no' command to accept an optional --times argument, allowing it to print 'no' a specified number of times.

--- no.sh
+++ no.sh
@@ -1,7 +1,36 @@
 #!/bin/sh
+# Author: ihatemustard
+# Simple "no" command with optional --times
 
-if [ "$#" -eq 0 ]; then
-    exec yes n
+times=-1
+
+# Parse arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --times)
+            shift
+            times="$1"
+            ;;
+        *)
+            echo "Usage: $0 [--times number]"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+print_no() {
+    echo "no"
+}
+
+if [ "$times" -eq -1 ]; then
+    while true; do
+        print_no
+    done
 else
-    exec yes "$*"
+    i=0
+    while [ $i -lt "$times" ]; do
+        print_no
+        i=$((i + 1))
+    done
 fi
